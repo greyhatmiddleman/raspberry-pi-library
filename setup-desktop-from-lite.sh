31b41f897c47e9060a5ae204cc64bb9740b3bcb2
pi4 <pi4@localhost.localdomain>
2021-01-06 16:03:06 +0000

    update desktop setup

--- setup-desktop-from-lite.sh
+++ setup-desktop-from-lite.sh
@@ -1,8 +1,16 @@
 #!/bin/env bash
 
-sudo apt install -y xserver-xorg
-sudo apt install -y raspberry-ui-mods
-sudo apt isntall -y lightdm
+sudo apt install -y xserver-xorg && \
+	sudo apt install -y raspberry-ui-mods && \
+	sudo apt install -y lightdm
 
 sudo apt install -y xrdp
-sudo reboot
+
+systemctl show -p SubState --value xrdp
+
+sudo sed -i 's/^allowed_user.*/allowed_users=anybody/' /etc/X11/Xwrapper.config
+
+sudo adduser xrdp ssl-cert
+
+echo "recommend to reboot at this point"
+#sudo reboot
